ace0d45168a5cb3cec85b79cc12b5baac6a293db
brenoamin <brenomatos125@gmail.com>
2021-06-08 20:47:16 -0300

    Pedra, papel, tesooooooura.

---
+++ jokenpo.js
@@ -0,0 +1,35 @@
+function solucao(joao, andre) {
+if(joao=="PEDRA"){
+  if(andre=="TESOURA"){
+    console.log("JOAO")
+  }
+else if(andre=="PAPEL"){
+    console.log("ANDRE")
+  }
+else {
+    console.log("EMPATE")
+  }
+}
+if(joao=="PAPEL"){
+  if(andre=="PEDRA"){
+    console.log("JOAO")
+  }
+else if(andre=="TESOURA"){
+    console.log("ANDRE")
+  }
+else {
+    console.log("EMPATE")
+  }
+}
+if(joao=="TESOURA"){
+  if(andre=="PAPEL"){
+    console.log("JOAO")
+  }
+else if(andre=="PEDRA"){
+    console.log("ANDRE")
+  }
+else {
+    console.log("EMPATE")
+  }
+}
+}
