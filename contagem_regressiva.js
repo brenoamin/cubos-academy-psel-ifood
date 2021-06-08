d254e9ac7e3ffbd2dcbd3015399d5a72d96fd9b9
brenoamin <brenomatos125@gmail.com>
2021-06-08 20:12:42 -0300

    The bomb has been planted

---
+++ contagem_regressiva.js
@@ -0,0 +1,11 @@
+function solucao(numero) {
+    var i;
+    for (i = numero; i >= 0; i--) {
+      console.log(i);
+    }
+    console.log("KABUM");    
+    }
+    
+    function processData(input) {
+        solucao(parseInt(input, 10));
+    } 
